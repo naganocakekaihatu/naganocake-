92454e7f542a703833f15592369ed5962f547c20
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2024-05-18 07:25:01 +0000

    Add Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_16_075006) do
+ActiveRecord::Schema.define(version: 2024_05_17_020028) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "item_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
