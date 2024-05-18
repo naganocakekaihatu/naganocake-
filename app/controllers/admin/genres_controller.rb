04b1a1e0a15557626a8772b9a786dabbf8af6e6a
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-05-18 06:06:35 +0000

    Add Genre Item

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,47 @@
 module Admin
   class GenresController < ApplicationController
+    
     def index
+      @genre = Genre.new
+      @genres = Genre.all
     end
   
     def create
+      @genre = Genre.new(genre_params)
+      if @genre.save
+        flash[:notice] = "ジャンルを作成しました。"
+        redirect_to admin_genres_path
+      else
+        @genres = Genre.all
+        render :index
+      end
     end
-  
+    
     def edit
+      @genre = Genre.find(params[:id])
     end
   
     def update
+      @genre = Genre.find(params[:id])
+      if @genre.update(genre_params)
+        flash[:notice] = "ジャンルを更新しました。"
+        redirect_to admin_genres_path
+      else
+        render :edit
+      end
+    end
+    
+    def destroy
+      genre = Genre.find(params[:id])
+      genre.destroy
+      flash[:notice] = "ジャンルを削除しました。"
+      redirect_to admin_genres_path
+    end
+    
+    private
+
+    def genre_params
+      params.require(:genre).permit(:name)
     end
   end
 end
