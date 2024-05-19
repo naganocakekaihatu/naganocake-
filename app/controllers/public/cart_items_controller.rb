9bd9f48ff3d6a6b21f63ecaed8207f637148a837
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2024-05-19 05:56:16 +0000

    カート追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,36 @@
 module Public
   class CartItemsController < ApplicationController
     def index
+      @cart_item = CartItem.all
     end
-  
+
     def update
     end
-  
+
     def destroy
     end
-  
+
     def destroy_all
     end
-  
+
     def create
+      cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer
+    cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
     end
+    end
+
+    private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2024_05_18_025652) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
-    t.text "address"
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-  
-end  
-  
-ActiveRecord::Schema.define(version: 2024_05_17_060430) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +40,23 @@ ActiveRecord::Schema.define(version: 2024_05_17_060430) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "postal_code"
+    t.text "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "item_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,6 +69,21 @@ ActiveRecord::Schema.define(version: 2024_05_17_060430) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.integer "price"
+    t.text "introduction"
+    t.boolean "is_active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
@@ -76,21 +94,6 @@ ActiveRecord::Schema.define(version: 2024_05_17_060430) do
     t.integer "total_payment"
     t.integer "payment_method"
     t.integer "status"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-
-  create_table "items", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "name"
-    t.integer "price"
-    t.text "introduction"
-    t.boolean "is_active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
