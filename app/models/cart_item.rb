21ac5b29b64ec2e5639c0b905e191f15f46e7b8a
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-05-19 10:24:04 +0000

    Add CartItem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,13 @@
 class CartItem < ApplicationRecord
-  belongs_to :end_user
+  belongs_to :customer
   belongs_to :item
-
+  
+  def with_tax_price
+    (item.price * 1.1).floor
+  end
+  
   def subtotal
-    item.with_tax_price * amount
+    with_tax_price * amount
   end
 
 end
