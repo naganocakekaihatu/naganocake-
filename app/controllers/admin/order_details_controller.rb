b5ccfc8c671e43de408be33ddee070ebe92743fc
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-05-22 04:05:44 +0000

    admin側注文履歴関連

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,16 @@
 # module Admin ←モデル名Adminが存在し、名前がブッキングしてエラーになるのでコメントアウトしてます
-  class Admin::OrderDetailsController < ApplicationController
-    def update
-    end
+class Admin::OrderDetailsController < ApplicationController
+  
+  def update
+   @order_detail = OrderDetail.find(params[:id])
+   @order_detail.update(order_detail_params)
+   redirect_to admin_order_path(@order_detail.order)
   end
+
+
+private
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+end
 # end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  has_many :order_details
   
   validates :genre_id, presence: true
   validates :name, presence: true
