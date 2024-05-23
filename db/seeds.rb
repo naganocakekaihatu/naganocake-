c7547c7fedb85692c5292555dd9e0b03f6025618
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2024-05-23 09:21:44 +0000

    テストの×の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(
-#   email: 'a@gmail.com',
-#   password: '000000'
-# )
+Admin.create!(
+  email: 'a@gmail.com',
+  password: '000000'
+)
